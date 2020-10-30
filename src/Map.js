a7b76c71a57ab41f43b91bf253f27da97c81644d
antonis-adraktas <72402130+antonis-adraktas@users.noreply.github.com>
2020-10-30 16:59:25 +0200

    added icon

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Map as LeafletMap, TileLayer, Marker } from 'react-leaflet'
 import Popup from 'react-leaflet-editable-popup'
+import blueIcon from './Icons'
 
 
 class Map extends React.Component{
@@ -8,8 +9,8 @@ class Map extends React.Component{
     constructor() {
         super()
         this.state = {
-            lat: 51.505,
-            lng: -0.09,
+            lat: 38.0018,
+            lng: 23.7423,
             zoom: 13
         }
     }
@@ -17,23 +18,23 @@ class Map extends React.Component{
     render(){
         const position = [this.state.lat, this.state.lng];
         return (
-            <LeafletMap doubleClickZoom={false} id="mapId" zoom={4} center={[33.852169, -100.5322]}  >
+            <LeafletMap doubleClickZoom={false} id="mapId" zoom={13} center={[38.0018, 23.7423]}  >
 
                 <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <Marker position={position}>
+                <Marker position={position} icon={blueIcon}>
                     <Popup maxWidth="600" removable editable >
                         Removable editable popup
                     </Popup>
                 </Marker>
 
-                <Marker position={[20.96176, -117.03529]}  >
+                <Marker position={[20.96176, -117.03529]} icon={blueIcon}  >
                     <Popup removable>
                         A removable popup
                     </Popup>
                 </Marker>
 
-                <Marker position={[25.86176, -112.03529]}>
+                <Marker position={[25.86176, -112.03529]} icon={blueIcon}>
                     <Popup maxWidth="500" editable>
                         Editable popup
                     </Popup>
