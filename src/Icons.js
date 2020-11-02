0a053a7ab70ff8bc6f0c38d86be0fb01ec931e7c
antonis-adraktas <72402130+antonis-adraktas@users.noreply.github.com>
2020-11-02 14:18:19 +0200

    added red icon

--- src/Icons.js
+++ src/Icons.js
@@ -8,3 +8,12 @@ export const blueIcon = new L.Icon({
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
 });
+
+export const redIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
